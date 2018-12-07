1b63c0bf85f0cf888b6c7d691b6cb84c5be36d8f
Marco Geier <mgeier63@gmail.com>
2018-12-07 16:39:22 +0700

    added additional encoding related texts by "freezing" current encoding

--- crypto/src/test/java/io/oversec/one/crypto/encoding/AsciiArmouredGpgXCoderTest.java
+++ crypto/src/test/java/io/oversec/one/crypto/encoding/AsciiArmouredGpgXCoderTest.java
@@ -20,4 +20,34 @@ public class AsciiArmouredGpgXCoderTest extends EncodingTestBase {
         assertTrue(msgOut.getMsgTextGpgV0().getPubKeyIdV0List().isEmpty()); //AsciiArmour encoding strips this info!
     }
 
+
+    @Test
+    public void testDecodePreKotlinization() throws Exception {
+        String encoded = "-----BEGIN PGP MESSAGE-----\n" +
+                "Charset: utf-8\n" +
+                "\n" +
+                "QWluJ3Qgd29ycmllZCBhYm91dCBub2JvZHkgZWxzZSBhbmQgbm9ib2R5IGNhbWUg\n" +
+                "YmV0d2VlbiB1cywgbm8gb25lIGNvdWxkIGV2ZXIgY29tZSBhYm92ZSBidXQgaSdk\n" +
+                "IHJhdGhlciB3b3JrIG9uIHRoaXMgd2l0aCB5b3UgY2F1c2UgaSBkb250IHdhbnQt\n" +
+                "d2FudCBub2JvZHkgd2hlbiBpIGdvdC1nb3QgeW91ciBib2R5IGRvbid0IGxldCB0\n" +
+                "aGlzIGdvIHRvIHlvdXIgaGVhZCBpIGFpbid0IG5ldmVyIHNlZW4gbm90aGluZyBs\n" +
+                "aWtlIHRoYXQgaSB3YW5uYSBrbm93IGlmIHlvdSBmZWVsaW5nLCB0aGUgd2F5IHRo\n" +
+                "YXQgaSdtIGZlZWxpbicgaSdtIGhlcmUgdG8gbWFrZSB5b3UgaGFwcHksIGknbSBo\n" +
+                "ZXJlIHRvIHNlZSB5b3Ugc21pbGUgaSd2ZSBiZWVuIHdhaXRpbmcgZm9yIGEgZ2ly\n" +
+                "bCBsaWtlIHlvdSBmb3IgYSBsb25nLCBsb25nLCBsb25nLCBsb25nIHRpbWUgbm93\n" +
+                "IG9oIHlvdXIgbG92ZSwgb2ggb3VyIHRydXN0IGhhcyBiZWVuIGJyb2tlbiBzaGFy\n" +
+                "ZSBzdGF5IGluIG15IGJhY2twYWNrIGZvcmV2ZXIgdGFrZSBhIGJvdywgeW91J3Jl\n" +
+                "IG9uIHRoZSBob3R0ZXN0IHRpY2tldCBub3cgKGVoIGVoIGVoKSB0aGVyZSdzIGEg\n" +
+                "ZHJlYW0gdGhhdCBpJ3ZlIGJlZW4gY2hhc2luZyB3ZSBhcmUgZ29ubmEgbWFrZSBp\n" +
+                "dCBnaXJsIHdoZW4gaSB3YXMgbGl0dGxlIHlvdSBhaW4ndCBnb3R0YSBiZSBhZnJh\n" +
+                "aWQu\n" +
+                "=XhVo\n" +
+                "-----END PGP MESSAGE-----\n" +
+                "\n";
+
+        Outer.Msg msgOut = mCoder.decode(encoded);
+
+        assertEquals(msgOut.getMsgTextGpgV0().getCiphertext().toStringUtf8(), CONTENT);
+        assertTrue(msgOut.getMsgTextGpgV0().getPubKeyIdV0List().isEmpty()); //AsciiArmour encoding strips this info!
+    }
 }
